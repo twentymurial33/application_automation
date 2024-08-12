c42d48f75f457bb5863a165ad1c60f50211f35de
Murial Anindo <murial.anindo@lonett.com>
2024-08-11 20:49:49 -0400

    updated header link

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,1 +1,15 @@
-export default async function upload(file) {}
+// import multer from "multer";
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: "./public/uploads",
+//     filename: (req, file, cb) => cb(null, file.originalname),
+//   }),
+// });
+
+// console.log(upload);
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
