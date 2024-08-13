fef62f032b37695d9c50dfbf92bc23553dc5c34d
Murial Anindo <murial.anindo@lonett.com>
2024-08-13 16:40:03 -0400

    new pr

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,15 +1,1 @@
-// import multer from "multer";
 
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: "./public/uploads",
-//     filename: (req, file, cb) => cb(null, file.originalname),
-//   }),
-// });
-
-// console.log(upload);
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
